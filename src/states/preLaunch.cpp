f46409ec9e2f9ec00117377a761d6748352bdeb9
Daniele Bella <danielebella2000@gmail.com>
2021-10-16 01:04:58 +0100

    Added acceleration triggering

--- src/states/preLaunch.cpp
+++ src/states/preLaunch.cpp
@@ -14,6 +14,7 @@ void preLaunch::initialise()
   ACCEL_THRESHOLD = 1.0;
 };
 
+<<<<<<< HEAD
 State *preLaunch::update()
 {
   float zAcc = 0;
@@ -46,6 +47,43 @@ State *preLaunch::update()
   // }
   return new flight(_sm);
   delay(500);
+=======
+State* preLaunch::update() {
+    float zAcc = 0;
+    static float accelAvg = 0;
+    static uint8_t counter = 0;
+    static bool reading = false;
+
+    _sm -> filter.reset();
+
+    if (_sm -> imu.accelAvailable())
+    {
+      _sm -> imu.readAccel();
+      //float xAcc = _sm -> imu.calcAccel(imu.ax);
+      //float yAcc = _sm -> imu.calcAccel(imu.ay);
+      zAcc = _sm -> imu.calcAccel(_sm -> imu.az); //TODO: use calibrated?
+    }
+
+    if (reading ==  true) {
+      accelAvg += zAcc;
+      counter++;
+    } else if (zAcc > ACCEL_THRESHOLD) {
+      counter = 0;
+      reading = true;
+    }
+
+    if (counter >= AVG_CTR) {
+      counter = 0;
+      reading = false;
+      if (accelAvg/AVG_CTR > ACCEL_THRESHOLD) {
+        return new flight(_sm);
+      } else {
+        return this;
+      }
+    } else {
+      return this;
+    }
+>>>>>>> 4077fbb (Added acceleration triggering)
 };
 
 void preLaunch::exitState(){
